e8bb039780f39f645b13f2c9106b66aaf0c32bed
Alfredo08 <alfredosv08@gmail.com>
2024-10-25 08:44:35 -0600

    Fixing migrations

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -142,5 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
